ba292f94bc250a7cb81d99e1af42271c6ff92ad6
Deimantas Abelskis <deimantas.abelskis@nfq.lt>
2018-07-30 11:47:35 +0300

    update for Arturas

--- Command/InitializeCommand.php
+++ Command/InitializeCommand.php
@@ -1,3 +1,23 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\RbacBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * @author Paweł Jędrzejewski <pawel@sylius.org>
+ */
 class InitializeCommand extends ContainerAwareCommand
 {
     private $rbacInitializer;
